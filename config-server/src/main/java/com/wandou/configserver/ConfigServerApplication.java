2b5db024c9e8ca01efcff15b68e98a87cf5d2ef1
wandou <liming8199@163.com>
2018-09-14 10:16:51 +0800

    9f xiazai

--- config-server/src/main/java/com/wandou/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/wandou/configserver/ConfigServerApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 2018-9-14在9f ThinkPad 下载此项目
+ */
+
 @EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
